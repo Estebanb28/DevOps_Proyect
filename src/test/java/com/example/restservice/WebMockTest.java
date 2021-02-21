43ae8d826cf5aad27bcd0c53752ed553f11aeb2e
Estebanb28 <Estebanb28@users.noreply.github.com>
2021-02-21 13:04:34 -0600

    Commit de correccion en pruebas unitarias

--- src/test/java/com/example/restservice/WebMockTest.java
+++ src/test/java/com/example/restservice/WebMockTest.java
@@ -1,21 +1,18 @@
 package com.example.restservice;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.contains;
+//import static org.hamcrest.Matchers.containsString;
+//import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.jayway.jsonpath.JsonPath;
 
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
